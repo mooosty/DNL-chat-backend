758205ee97da70a88df06e08e2da498635899200
khurram44 <ks7844201@gmail.com>
2024-09-09 00:39:06 +0500

    user setup

---
+++ email/index.js
@@ -0,0 +1,75 @@
+
+const nodemailer = require("nodemailer")
+const Hash = require("../models/hash")
+const bcrypt = require("bcrypt")
+const sendNewMail = async (email, id) => {
+    const hash = makeid(100) + `&&ID=${id}`
+    console.log(`http://localhost:3000/reset-password/${hash}`)
+    // let transporter = nodemailer.createTransport({
+    //     host: process.env.HOST,
+    //     port: 465,
+    //     secure: true,
+    //     requireTLS: true,
+    //     auth: {
+    //         user: process.env.EMAIL,
+    //         pass: process.env.PASSWORD
+    //     }
+    // });
+
+    // let mailOptions = {
+    //     from: 'no-reply@devstax.org',
+    //     to: email,
+    //     subject: 'Password-Reset',
+    //     html: `<div>
+    //        <h1>Here is the Link to Update your password.</h1>
+    //        <p>This is your one time activation link. It will be expired in a while</p>
+    //        <a href=${`http://localhost:3000/reset-password/${hash}`} target="_blank" >Reset Password</a>
+           
+    //     </div>`
+    // };
+
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //     if (error) {
+    //         console.log(error.message)
+    //         return 0;
+    //     }
+    //     else {
+    //         console.log("success")
+
+    //         return 1
+    //     }
+    // });
+    try {
+        await Hash.findOneAndUpdate(
+            { userid: id },
+            {
+                $set: {
+                    hash: hash
+                },
+            },
+            { new: true }
+        );
+
+        return 1
+    }
+    catch (err) {
+        console.log(err)
+        return err
+
+    }
+
+
+
+}
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+module.exports = {sendNewMail}
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { getUsers, createUser, loginUser } = require("../controllers/user")
+const { getUsers, createUser, loginUser, sendRecoveryLink, activateRecoveryLink, resetPassword, getHash } = require("../controllers/user")
 const { protect } = require("../middleware/authProtection")
 const router = require("express").Router()
 
@@ -6,6 +6,10 @@ const router = require("express").Router()
 router.get("/all",protect,getUsers)
 router.post("/create",createUser)
 router.post("/login",loginUser)
+router.post("/send-recovery-link",sendRecoveryLink)
+router.post("/activate-recovery-link",activateRecoveryLink)
+router.post("/reset-password",resetPassword)
+router.post("/hash",getHash)
 
 
 module.exports = router
